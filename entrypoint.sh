ea577cfaa27a36478eef8b2fe65c7b34b40edc3b
Thomas Fricke <github@thomasfricke.de>
2022-04-12 12:58:36 +0200

    now bash_completion includedwq

--- entrypoint.sh
+++ entrypoint.sh
@@ -9,8 +9,8 @@ then
     ln -s $i  $dir
   done
   echo export 'PATH=$PATH:$(echo $PATH | sed s+^+/host+ |  sed "s+\:+\:/host+g"):/usr/local/go/bin' > /root/.bash_aliases 
-  source /usr/share/bash-completion/bash_completion
-  for i in docker kubectl helm ; do $i completion bash > /etc/bash_completion.d/$i-prompt ; done
+  echo source /usr/share/bash-completion/bash_completion >> /root/.bash_aliases
+  for i in crictl docker kubectl helm ; do $i completion bash > /etc/bash_completion.d/$i.sh ; done
   source /root/.bash_aliases
 else
   echo "no /host dir found"
