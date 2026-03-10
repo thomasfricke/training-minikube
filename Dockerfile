75cbff08d97ebfcb05a4430bb401d84fb5e4a7de
Thomas Fricke <github@thomasfricke.de>
2026-03-10 15:53:51 +0100

    clean up to run everything in an existing cluster as privileged container. Provides example for k3s

--- Dockerfile
+++ Dockerfile
@@ -28,5 +28,9 @@ ADD jupyter_notebook_config.py /etc/jupyter/jupyter_notebook_config.py
 
 ADD entrypoint.sh /entrypoint.sh
 
+RUN curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl" && chmod +x kubectl && mv kubectl /usr/bin
+
+
+
 ENTRYPOINT ["/entrypoint.sh"]
 
--- entrypoint.sh
+++ entrypoint.sh
@@ -21,6 +21,7 @@ cd /notebooks
 JUPYTERLAB_PORT="${JUPYTERLAB_PORT:-8888}"
 JUPYTERLAB_LOGLEVEL="${JUPYTERLAB_LOGLEVEL:-INFO}" 
 JUPYTERLAB_WELCOME="${JUPYTERLAB_WELCOME:-/notebooks/Welcome.ipynb}"
+JUPYTERLAB_IP="${JUPYTERLAB_IP:-0.0.0.0}"
 
-/usr/local/bin/jupyter-lab --allow-root --port=$JUPYTERLAB_PORT --log-level=$JUPYTERLAB_LOGLEVEL $JUPYTERLAB_WELCOME --IdentityProvider.token=$JUPYTERLAB_TOKEN
+/usr/local/bin/jupyter-lab --allow-root --port=$JUPYTERLAB_PORT --ip="${JUPYTERLAB_IP}" --log-level=$JUPYTERLAB_LOGLEVEL $JUPYTERLAB_WELCOME --IdentityProvider.token=$JUPYTERLAB_TOKEN
 
